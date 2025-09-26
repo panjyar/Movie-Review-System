7c98632a246bccc737eda8204d184920f3b7feb5
panjyar <panjiyarkehuli@gmail.com>
2025-09-27 00:22:23 +0530

    production : path-to-regexp update.

--- server/server.js
+++ server/server.js
@@ -76,9 +76,10 @@ app.get('/api/health', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   
-app.get('/:path*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 });
+
 }
 
 
