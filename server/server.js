25b5f592940e9d29b4eed625c2cc28f2e90a769f
panjyar <panjiyarkehuli@gmail.com>
2025-09-28 20:52:56 +0530

    production: added /* routes to fix path-to-regexp

--- server/server.js
+++ server/server.js
@@ -76,7 +76,7 @@ app.get('/api/health', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 });
 
